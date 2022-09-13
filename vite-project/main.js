2937d0ac6da1ece400e323c3cac898d3b97270a1
Harsh Valaki <96899883+hxrsh7@users.noreply.github.com>
2022-09-13 12:08:24 +0530

    ✨ Stars added

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,7 +1,8 @@
 import './style.css'
-
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+// Camera and Renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -12,11 +13,41 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-const geometry = new THREE.TorusKnotGeometry( 7.5, 2.5, 100, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0x24ff99, wireframe: true } );
+// Torus Knot
+const geometry = new THREE.TorusKnotGeometry( 7.5, 2.5, 32, 8 );
+const material = new THREE.MeshStandardMaterial( { color: 0x808080, wireframe: true });
 const torusKnot = new THREE.Mesh( geometry, material );
 scene.add( torusKnot );
 
+// Lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambientLight =  new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+// Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Stars
+function addStar(){
+  const geometry = new THREE.TetrahedronGeometry(0.25)
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff, wireframe: true } );
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+// Animate
 function animate(){
   requestAnimationFrame( animate );
 
